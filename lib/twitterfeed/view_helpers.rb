f95f641260b596923e8ed3beadeaeb007a857d8a
Mark Capodagli <pimea.mark@gmail.com>
2012-05-09 09:06:44 -0500

    fixed problem with initial use of twitterfeed

--- lib/twitterfeed/view_helpers.rb
+++ lib/twitterfeed/view_helpers.rb
@@ -4,13 +4,13 @@ module Twitterfeed
   module ViewHelpers
     def twitterfeed(name_array)
       
-      twitterfeed_data = YAML.load(File.open("twitterfeed.yml"))
+      twitterfeed_data = YAML.load(File.open("twitterfeed.yml", 'w+'))
       
       #initializing cron type system. This will determine how often data is retrieved from twitter to not go over
       #Twitter API's 150 request per hour limit. The frequency is determined from how many twitter handles are being
       #retrieved. 1 handle will be retrieved maximum of 150 times per hour etc.
       
-      if twitterfeed_data.first["last_update"] < (Time.now - (60.0 * (60.0/(150.0/name_array.size))))
+      if !twitterfeed_data || twitterfeed_data.first["last_update"] < (Time.now - (60.0 * (60.0/(150.0/name_array.size))))
         Twitterfeed.update_twitterfeed(twitterfeed_data, name_array)
       end  
       
